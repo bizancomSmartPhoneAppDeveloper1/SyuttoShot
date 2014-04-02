d72386a2a5227aa812983d051621d6a9289b07cb
shinoarita <shinoarita@gmail.com>
2014-04-02 18:45:02 +0900

    Initial Commit

---
+++ SyuttoShot/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SyuttoShot
+//
+//  Created by bizan.com.mac09 on 2014/04/02.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SyuttoShot/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SyuttoShot
+//
+//  Created by bizan.com.mac09 on 2014/04/02.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
